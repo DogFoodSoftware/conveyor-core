6c951a3ebfec2058aa287a4a1dff8a40f6e3773c
Zane Rockenbaugh <zane@liquid-labs.com>
2013-11-04 17:20:26 +0000

    add 'Implementation' section

--- src/rest/plans/get.php
+++ src/rest/plans/get.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * <div id="Implementation" class="blurbSummary grid_12">
+ */
 $document_url_path='plans';
 require_once '/home/user/playground/kwiki/runnable/rest/documentation/get.php';
+/**
+ * </div><!-- #Implementation.blurbSummary -->
+ */
 ?>
