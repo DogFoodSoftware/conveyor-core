8988ce2c44ec9058bc080b35ab251aed2bc9b17c
Zane Rockenbaugh <zane@liquid-labs.com>
2014-01-04 18:57:02 +0000

    point commit in new install process

---
+++ conf/conveyor-project-dependencies.sh
@@ -0,0 +1,1 @@
+REQUIRED_PROJECTS="https://github.com/DogFoodSoftware-Third-Party/apache-httpd.git"
---
+++ conf/default-config.sh
@@ -0,0 +1,5 @@
+# 1.0 may support relocatable playground, but for 0.1, our target is support
+# of a isolated VM with a user 'user' and known playground dir.
+export CONVEYOR_PLAYGROUND=/home/user/playground
+export DFS_HOME=$CONVEYOR_PLAYGROUND/dogfoodsoftware.com
+export CONVEYOR_HOME=$DFS_HOME/conveyor
