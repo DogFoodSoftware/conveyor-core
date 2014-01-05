908234be6f4b179b9f7293a4b3f1041a77653570
Zane Rockenbaugh <zane@liquid-labs.com>
2014-01-05 21:54:01 +0000

    added dependencies for kwiki

--- conf/conveyor-project-dependencies.sh
+++ conf/conveyor-project-dependencies.sh
@@ -6,5 +6,8 @@ REQUIRED_PROJECTS="\
 ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/third-party/shflags.git \
 ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/third-party/apache-httpd.git \
 ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/third-party/postgres.git \
-ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/third-party/php5.git" # \
-#ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/conveyor-rest/kwiki.git"
+ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/third-party/php5.git \
+ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/jquery/element-docker.git \
+ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/jquery/page-browser.git \
+ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/jquery/perspectives.git \
+ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/conveyor-rest/kwiki.git"
