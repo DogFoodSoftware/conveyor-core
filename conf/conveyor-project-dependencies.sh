19d9b6aae4475c992e501c301dc3e74da38e9240
Zane Rockenbaugh <zane@liquid-labs.com>
2014-01-05 01:54:04 +0000

    first draft new setup

--- conf/conveyor-project-dependencies.sh
+++ conf/conveyor-project-dependencies.sh
@@ -1,1 +1,7 @@
-REQUIRED_PROJECTS="https://github.com/DogFoodSoftware-Third-Party/apache-httpd.git"
+# REQUIRED_PROJECTS="https://github.com/DogFoodSoftware-third-party/apache-httpd.git \
+# https://github.com/DogFoodSoftware-third-party/php5.git \
+# https://github.com/DogFoodSoftware-conveyor-rest/kwiki.git"
+
+REQUIRED_PROJECTS="ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/third-party/apache-httpd.git \
+ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/third-party/php5.git \
+ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/conveyor-rest/kwiki.git"
