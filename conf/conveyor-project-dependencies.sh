100a30abe6df4cb4ada3dd07b6809227e17fa5de
Zane Rockenbaugh <zane@liquid-labs.com>
2014-01-05 02:12:58 +0000

    need space before #

--- conf/conveyor-project-dependencies.sh
+++ conf/conveyor-project-dependencies.sh
@@ -2,6 +2,6 @@
 # https://github.com/DogFoodSoftware-third-party/php5.git \
 # https://github.com/DogFoodSoftware-conveyor-rest/kwiki.git"
 
-REQUIRED_PROJECTS="ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/third-party/apache-httpd.git"# \
+REQUIRED_PROJECTS="ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/third-party/apache-httpd.git" # \
 #ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/third-party/php5.git \
 #ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/conveyor-rest/kwiki.git"
