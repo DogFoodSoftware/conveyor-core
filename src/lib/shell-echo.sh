6afac71bd4aef51e4c9787ecf76a1714e80e9e26
Zane Rockenbaugh <zane@liquid-labs.com>
2014-01-07 00:24:25 +0000

    fleshed out documentation for script

--- src/lib/shell-echo.sh
+++ src/lib/shell-echo.sh
@@ -1,11 +1,25 @@
 # /**
 # * <div id="Overview" class="blurbSummary">
+# *   <div class="p">
 # *   Library providing drop in support for 'quiet' and 'verbose' output in
-# *   shell scripts. <span data-perspective="coding">The script will enable
-# *   the <code>-q/--quiet</code> and <code>-v/--verbose</code>
-# *   options. Relies on the including script to invoke the argument
-# *   processing prior to invoking the methods.</span>
-#* </div><!-- #Overview.blurbSummary -->
+# *   shell scripts. The basic ideas is computers generally like very quite
+# *   scripts while humans prefer chatty scripts, and by using this library,
+# *   you're setting up to write a script that supports that idea.
+# *   </div>
+# *   <div class="p" data-perspective="coding">
+# *   The general idea is that scripts primarily run by humans&mdash;CLI
+# *   enablers&mdash;need to be quieted when run by automated processes and
+# *   scripts designed primarily for automated processes should become more
+# *   chatty when run by a human. To implement that, include
+# *   <code>shflags</code> in your code, include this script, and then <a
+# *   href="https://code.google.com/p/shflags/wiki/Documentation10x">process
+# *   the flag options</a>. This script will enable the
+# *   <code>-q/--quiet</code> and <code>-v/--verbose</code> options. Then, use
+# *   'qecho' and 'vecho' for output as appropriate. Generally, you will stil
+# *   use <code>echo</code> for all "functional messages" which should be
+# *   displayed no matter what.
+# *   </div>
+# * </div><!-- #Overview.blurbSummary -->
 # */
 
 DEFINE_boolean 'quiet' $FLAGS_FALSE 'Suppresses "merely informative" output.' 'q'
