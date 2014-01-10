6f23f13f0a5a38e4bd740a0d5c7e28724f72ae1f
Zane Rockenbaugh <zane@liquid-labs.com>
2014-01-10 03:55:29 +0000

    all dependencies now migrated

--- conf/conveyor-project-dependencies.sh
+++ conf/conveyor-project-dependencies.sh
@@ -3,7 +3,7 @@ https://github.com/DogFoodSoftware/_third-party-shflags.git \
 https://github.com/DogFoodSoftware/_third-party-apache-httpd.git \
 https://github.com/DogFoodSoftware/_third-party-postgres.git \
 https://github.com/DogFoodSoftware/_third-party-php5.git \
-ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/jquery/element-docker.git \
-ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/jquery/page-browser.git \
+https://github.com/DogFoodSoftware/jquery-element-docker.git \
+https://github.com/DogFoodSoftware/jquery-page-browser.git \
 https://github.com/DogFoodSoftware/jquery-perspectives.git \
-ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/conveyor/kwiki.git"
+https://github.com/DogFoodSoftware/conveyor-kwiki.git"
