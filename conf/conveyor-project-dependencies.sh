9123554e87a3654b6d66c9b638883e5311ada761
Zane Rockenbaugh <zane@liquid-labs.com>
2014-01-10 00:03:15 +0000

    update dependencies for new PHP wrapper home

--- conf/conveyor-project-dependencies.sh
+++ conf/conveyor-project-dependencies.sh
@@ -2,7 +2,7 @@ REQUIRED_PROJECTS="\
 https://github.com/DogFoodSoftware/_third-party-shflags.git \
 https://github.com/DogFoodSoftware/_third-party-apache-httpd.git \
 https://github.com/DogFoodSoftware/_third-party-postgres.git \
-ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/third-party/php5.git \
+https://github.com/DogFoodSoftware/_third-party-php5.git \
 ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/jquery/element-docker.git \
 ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/jquery/page-browser.git \
 ssh://zane@git.liquid-labs.com/home/user/playground/git/data/dogfoodsoftware.com/jquery/perspectives.git \
