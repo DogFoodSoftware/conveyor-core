f48ba4a2aa6666aec736a8210c2d18b77fadcc31
Zane Rockenbaugh <zane@liquid-labs.com>
2014-01-14 02:53:26 +0000

    added todo since we can't do the 'git check-ignore' right now

--- src/lib/service-lib-uninstall-by-runnable.sh
+++ src/lib/service-lib-uninstall-by-runnable.sh
@@ -5,6 +5,12 @@ source $CONVEYOR_HOME/runnable/lib/shell-echo.sh
 
 RUNNABLE=$CONVEYOR_PLAYGROUND/$CONVEYOR_PROJECT_HOME/runnable
 
+# TODO: we would love to use 'git check-ignore runnable/' here and cowardly
+# refuse to delete unless there's a match. Implement '--force' option to skip
+# check. The problem is 'check-ignore' was not implemented till git
+# 1.8.2. Since we could potentially be on openSuSE 13.1 for awhile, there are
+# other workarounds we could do till
+# then. http://stackoverflow.com/questions/466764/show-ignored-files-in-git
 if [[ -d $RUNNABLE ]]; then
     rm -rf $RUNNABLE
     vecho "$PROJECT_NAME successfully uninstalled."
