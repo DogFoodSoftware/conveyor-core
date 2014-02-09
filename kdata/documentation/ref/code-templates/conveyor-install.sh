c18f89c29db5a2ebfc8847144fa7edc496b46a5e
Zane Rockenbaugh <zane@liquid-labs.com>
2014-02-09 00:58:44 +0000

    point commit

---
+++ kdata/documentation/ref/code-templates/conveyor-install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+##### TO USE THIS FILE:
+##
+## 1) Copy to root of project home directory.
+## 2) Find and change all instances of 'CHANGEME' in the text.
+## 3) Remove this notice.
+## 4) Implement project specific implementation logic, including but not
+##    limited to the 'IMPLEMENTME' notes.
+## 5) Ensure all instances of 'IMPLEMENTME' and 'CHANGEME' have been removed.
+
+# /**
+# * <div id="Overview" class="blurbSummary grid_12">
+# * <div class="p">
+# * Conveyor compatible install script for CHANGEME. Based on the <a
+# * href="http://dogfoodsoftware.com/documentation/conveyor/ref/code-templates/conveyor-install.sh">standard
+# * template</a>. The installation script&mdash;which conforms to the <a
+# * href="/documentation/conveyor-core/ref/Projects#Custom-Installation">Conveyor
+# * standard</a>&mdash;is included in order to CHANGEME(explain why the script is necessary and what it does beyond the .
+# * </div>
+# * </div><!-- #Overview.blurbSummary -->
+# * <div id="Implementation" class="blurbSummary grid_12">
+# * <div class="blurbTitle">Implementation</div>
+
+# * <div class="p">
+
+
+# * </div>
+# * </div><!-- #Implementation.blurbSummary -->
+# */
