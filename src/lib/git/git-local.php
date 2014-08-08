7fc59c9c5832f8ee2376c32f2871197f257c9029
Zane Rockenbaugh <zane@liquid-labs.com>
2014-08-08 19:09:37 +0000

    change system calls to exec

--- src/lib/git/git-local.php
+++ src/lib/git/git-local.php
@@ -1,13 +1,14 @@
 <?php
 function branch_create_local($repo_path, $branch_name) {
+    $output = array(); // throw away, but needed to extract return value from exec()
     $orig_working_dir = getcwd();
     chdir($repo_path);
-    system("git show-ref --verify --quiet 'refs/heads/$branch_name'", $retval);
+    exec("git show-ref --verify --quiet 'refs/heads/$branch_name'", $output, $retval);
     if ($retval == 0) {
         chdir($orig_working_dir);
         final_result_bad_request("Branch '$branch_name' already exists in repo file://{$repo_path}.");
     }
-    system("git branch $BRANCH_NAME", $retval);
+    exec("git branch $BRANCH_NAME", $output, $retval);
     chdir($orig_working_dir);
     if ($retval != 0) {
         final_result_system_error("Could not create branch in repo file://{$repo_path}.");
