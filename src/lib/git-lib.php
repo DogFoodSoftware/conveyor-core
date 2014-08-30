b9f428b137bcf309066453b25c7def6e798dea00
Zane Rockenbaugh <zane@liquid-labs.com>
2014-08-30 21:45:08 +0000

    point commit

--- src/lib/git-lib.php
+++ src/lib/git-lib.php
@@ -1,24 +1,43 @@
 <?php
-function branch_create($repo, $branch_name) {
-    # Regularize $repo; if no protocol, assume it's a file.
-    if (!preg_match('|^\w+://|', $repo)) {
-        $repo = 'file://'.$repo;
-    }
+function branch_create($repo_url, $branch_name) {
+    $repo_url = _regularize_repo_url($repo_url);
 
-    if (preg_match('|^file://|', $repo)) {
-        $path = substr($repo, 7, strlen($repo) - 7);
-
-        # All files must be absolute.
-        if (!preg_match('|^/|', $path)) {
-            final_result_bad_request("File repos must be absolute; got '$repo'.");
-        }
-
+    if (preg_match('|^file://|', $repo_url)) {
         require_once(dirname(__FILE__).'/git/git-local.php');
-        branch_create_local($path, $branch_name);
+        branch_create_local($repo_url, $branch_name);
     }
-    elseif (preg_match('|https?://github.com|', $repo)) {
+    elseif (preg_match('|https?://github.com|', $repo_url)) {
         require_once(dirname(__FILE__).'/git/git-github.php');
-        branch_create_github($repo, $branch_name);
+        branch_create_github($repo_url, $branch_name);
     }
+}
+
+function branch_checkout($repo_url, $branch_name) {
+    echo "branch_checkout:16:$repo_url\n";
+    $repo_url = _regularize_repo_url($repo_url);
+    echo "branch_checkout:18:$repo_url\n";
+
+    # We can count on the protocol being explicit after the
+    # regularization.
+    if (preg_match('|^file://|', $repo_url)) {
+        require_once(dirname(__FILE__).'/git/git-local.php');
+        branch_checkout_local($repo_url, $branch_name);
+    }
+    else {
+        final_result_bad_request("Operation 'checkout' incompatible with remote repositories.");
+    }
+}
+
+function _regularize_repo_url($repo_url) {
+    # At the time of writing, this 'file://' protocol will later be
+    # tripped off. But we want to keep the code simple while also
+    # allowing the user to cheat a little when the meaning is clear. So
+    # we first regularize, and then let the particular sub-handlers
+    # trust they're getting a common input.
+    if (!preg_match('|^\w+://|', $repo_url)) {
+        $repo_url = 'file://'.$repo_url;
+    }
+
+    return $repo_url;
 }
 ?>
--- src/lib/git/git-local.php
+++ src/lib/git/git-local.php
@@ -1,10 +1,15 @@
 <?php
-function branch_create_local($repo_path, $branch_name) {
+function branch_create_local($repo_url, $branch_name) {
+    echo "A";
+    $repo_path = _rac_repo_url($repo_url);
+    echo "B";
+
     $output = array(); // throw away, but needed to extract return value from exec()
     $orig_working_dir = getcwd();
     exec("cd '$repo_path' && git show-ref --verify --quiet 'refs/heads/$branch_name'", $output, $retval);
     if ($retval == 0) {
         chdir($orig_working_dir);
+        this_should_be_a_warning_do_we_support_that();
         final_result_bad_request("Branch '$branch_name' already exists in repo file://{$repo_path}.");
     }
     exec("cd '$repo_path' && git branch $branch_name", $output, $retval);
@@ -23,5 +28,35 @@ function branch_exists_local($branch_spec) {
 
     exec("git show-ref --verify --quiet '$branch_spec'", $output = array(), $retval);
     return $retval == 0;
+}
+
+function branch_checkout_local($repo_url, $branch_name) {
+    echo "C";
+    $repo_path = _check_repo_url($repo_url);
+    echo "D";
+
+    exec("cd '$repo_path' && git checkout '$branch_name'", 
+         $output = arary(), 
+         $retval);
+
+    if ($retval != 0) {
+        final_result_internal_error("Could not update local repository checkout. Bailing out.");
+    }
+}
+
+function _rac_repo_url($repo_url) {
+    // At this level, always a file, and we return more useful path.
+    $repo_path = $repo_url;
+    if (preg_match('|^file://|', $repo_url)) {
+        $repo_path = substr($repo_url, 7);
+    }
+    if (!preg_match('|^/|', $repo_path)) {
+        final_result_bad_request("Local repository path must be abosulte. Got '$repo_path'. Bailing out.");
+    }
+    if (!is_dir($repo_path)) {
+        final_result_bad_request("Local repository path '$repo_path' exists, but is not a directory. Bailing out.");
+    }
+
+    return $repo_path;
 }
 ?>
