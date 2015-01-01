9b42d109f2ba4ce8c66dd6c217cdcde90cbb2adf
Zane Rockenbaugh <zane@liquid-labs.com>
2015-01-01 23:11:30 +0000

    removed deprecated dependencies file and updated config (assuming it's still used) to use the new '/home/user' convention

--- conf/default-config.sh
+++ conf/default-config.sh
@@ -1,5 +1,5 @@
 # 1.0 may support relocatable playground, but for 0.1, our target is support
 # of a isolated VM with a user 'user' and known playground dir.
-export CONVEYOR_PLAYGROUND=/home/user/playground
-export DFS_HOME=$CONVEYOR_PLAYGROUND/dogfoodsoftware.com
-export CONVEYOR_HOME=$DFS_HOME/conveyor
+export CONVEYOR_PLAYGROUND="$HOME/playground"
+export DFS_HOME="$CONVEYOR_PLAYGROUND/dogfoodsoftware.com"
+export CONVEYOR_HOME="$DFS_HOME/conveyor"
