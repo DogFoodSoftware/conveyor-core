bf7d5095ae6ff011fc2fab734bf18ba51f9677d2
Zane Rockenbaugh <zane@liquid-labs.com>
2015-08-10 20:26:43 +0000

    ready to test default JS setup

--- conf/default.minify.php
+++ conf/default.minify.php
@@ -1,10 +1,10 @@
 <?php
 
 $files_dir = '/home/vagrant/data/files';
-$js = array("$files_dir/js/jquery-2.1.1.js",
-            "$files_dir/js/ICanHaz.js",
-            "$files_dir/js/bootstrap.js",
-            "$files_dir/js/jquery.sticky.js");
+$js = array("$files_dir/js/jquery-2.1.4.js",
+            "$files_dir/js/ICanHaz-0.10.2.js",
+            "$files_dir/js/bootstrap-3.3.5.js",
+            "$files_dir/js/jquery.sticky-1.0.3.js");
 $css = array("$files_dir/css/master.css");
 
 return array('default-js' => $js,
