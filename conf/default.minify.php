96315b53b6aff4f4e0f431c46802dd1a930c2f19
Zane Rockenbaugh <zane@liquid-labs.com>
2015-08-10 19:36:16 +0000

    updated return

--- conf/default.minify.php
+++ conf/default.minify.php
@@ -7,4 +7,6 @@ $js = array("$files_dir/js/jquery-2.1.1.js",
             "$files_dir/js/jquery.sticky.js");
 $css = array("$files_dir/css/master.css");
 
+return array('default-js' => $js,
+             'default-css' => $css);
 ?>
