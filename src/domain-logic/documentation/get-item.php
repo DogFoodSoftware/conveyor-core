6aca477e00a7939b1e35409682e42f2186d6e17d
Zane Rockenbaugh <zane@liquid-labs.com>
2015-08-24 22:22:34 +0000

    sketched in rudimentary support for directory

--- src/domain-logic/documentation/get-item.php
+++ src/domain-logic/documentation/get-item.php
@@ -9,41 +9,73 @@ require("lib/authorization-lib.php");
 
 $file_path = "/home/vagrant/documentation/$req_item_id";
 
+function human_out($in) {
+    $out = preg_replace('/-/', ' ', $in);
+    $out = preg_replace('/\.[^.]+$/', '', $out);
+    return $out;
+}
+
 if (!file_exists($file_path)) {
     $msg = "404: Did not find '$req_path'.";
     $response->set_data(array("document" => array('contents' => $msg)));
     $response->item_not_found();
 }
 else {
-    $document_contents = file_get_contents($file_path);
-    $document = array('contents' => $document_contents);
-    if (preg_match('/^\s*<!--\s+breadcrumb:\s*((\/?[\(\)\w |-]+))\s*-->\s*$/m', $document_contents, $matches)) {
-        $breadcrumb_spec = $matches[1];
-        if (!empty($breadcrumb_spec)) {
-            $breadcrumb = array();
-            $crumb_specs = explode('|', $breadcrumb_spec);
-            $document['title'] = array_pop($crumb_specs);
-            foreach ($crumb_specs as $crumb_spec) {
-                if (preg_match('/\(([^\)]+)\)?(.+)/', $crumb_spec, $matches)) {
-                    $crumb = array('path' => $matches[1],
-                                   'name' => $matches[2]);
-                    if (empty($crumb['path'])) {
-                        $crumb['path'] = $name;
+    if (is_dir($file_path)) {
+        if ($dh = opendir($file_path)) {
+            $document_contents .= "<ul>\n";
+            while (($file = readdir($dh)) !== false) {
+                if (!preg_match("/^\./", $file)) {
+                    if (is_dir("{$file_path}{$file}")) {
+                        $document_contents .= '<li><a href="'.$req_path.$file.'">'.human_out($file)."</a></li>\n";
                     }
-                    array_push($breadcrumb, $crumb);
+                    else {
+                        $document_contents .= '<li><a href="'.$req_path.'/'.$file.'">'.human_out($file)."</a></li>\n";
+                    }
                 }
-                // else add warning to response
             }
-            $document['breadcrumb'] = $breadcrumb;
-                            
+            closedir($dh);
+            $document_contents .= "</ul>\n";
+
+            $document = array('contents' => $document_contents);
+        }
+        else {
+            $msg = "404: Could not open directory-path '$file_path'.";
+            $response->set_data(array("document" => array('contents' => $msg)));
+            $response->item_not_found();
         }
     }
-    else { // No breadcrumb def, let's divine the title.
-        $document['title'] = basename($req_path);
+    else {
+        $document_contents = file_get_contents($file_path);    
+        $document = array('contents' => $document_contents);
+        if (preg_match('/^\s*<!--\s+breadcrumb:\s*((\/?[\(\)\w |-]+))\s*-->\s*$/m', $document_contents, $matches)) {
+            $breadcrumb_spec = $matches[1];
+            if (!empty($breadcrumb_spec)) {
+                $breadcrumb = array();
+                $crumb_specs = explode('|', $breadcrumb_spec);
+                $document['title'] = array_pop($crumb_specs);
+                foreach ($crumb_specs as $crumb_spec) {
+                    if (preg_match('/\(([^\)]+)\)?(.+)/', $crumb_spec, $matches)) {
+                        $crumb = array('path' => $matches[1],
+                                       'name' => $matches[2]);
+                        if (empty($crumb['path'])) {
+                            $crumb['path'] = $name;
+                        }
+                        array_push($breadcrumb, $crumb);
+                    }
+                    // else add warning to response
+                }
+                $document['breadcrumb'] = $breadcrumb;
+                
+            }
+        }
+        else { // No breadcrumb def, let's divine the title.
+            $document['title'] = basename($req_path);
+        }
     }
+
     $response->ok('Document retrieved.', array("document" => $document));
 }
-?>
-<?php /**
+/**
 </div><!-- .blurbSummary#Implementation -->
 */ ?>
