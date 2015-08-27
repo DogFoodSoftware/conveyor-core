6806907e44f10956523f5d08e99ac3720f4e2daa
Zane Rockenbaugh <zane@liquid-labs.com>
2015-08-27 21:28:34 +0000

    simplify loading of default js package

--- conf/default.minify.php
+++ conf/default.minify.php
@@ -1,11 +1,9 @@
 <?php
 
 $files_dir = '/home/vagrant/data/files';
-$js = array("$files_dir/js/jquery-2.1.4.js",
-            "$files_dir/js/ICanHaz-0.10.2.js",
-            "$files_dir/js/bootstrap-3.3.5.js",
-            "$files_dir/js/jquery.sticky-1.0.3.js");
-$css = array("$files_dir/css/master.css");
+$js = glob("$files_dir/js/*.js");
+error_log(print_r($js,TRUE));
+$css = glob("$files_dir/css/*.css");
 
 return array('default-js' => $js,
              'default-css' => $css);
