eac8d595ecd72b94f6227958d6456f0c68b068e5
Zane Rockenbaugh <zane@liquid-labs.com>
2015-09-15 21:56:36 +0000

    use the '/etc/environment' for DOCUMENTATION_HOME

--- src/lib/conveyor-libs.sh
+++ src/lib/conveyor-libs.sh
@@ -2,21 +2,23 @@ function con_doc_link {
     local SOURCE_ROOT="$1"
     local PATH_OFFSET="$2"
 
+    source /etc/environment # defines $DOCUMENTATION_HOME
+
     local SAVEIFS=$IFS
     IFS=$(echo -en "\n\b")
 
     for i in `ls $SOURCE_ROOT`; do
 	if [ -d "$SOURCE_ROOT/$i" ]; then
 	    PATH_OFFSET="$PATH_OFFSET/$i"
-	    if [ ! -d "$HOME/documentation/$i" ]; then
-		mkdir "$HOME/documentation/$i"
+	    if [ ! -d "$DOCUMENTATION_HOME/$i" ]; then
+		mkdir "$DOCUMENTATION_HOME/$i"
 	    fi
 	    con_doc_link "$SOURCE_ROOT/$i" "$PATH_OFFSET"
 	else
 	    echo "is link"
 	    IOFFSET="$PATH_OFFSET/$i"
-	    if [ ! -f "$HOME/documentation/$IOFFSET" ]; then
-		ln -s "$SOURCE_ROOT/$i" "$HOME/documentation$IOFFSET"
+	    if [ ! -f "$DOCUMENTATION_HOME/$IOFFSET" ]; then
+		ln -s "$SOURCE_ROOT/$i" "${DOCUMENTATION_HOME}$IOFFSET"
 	    fi
 	fi
     done
