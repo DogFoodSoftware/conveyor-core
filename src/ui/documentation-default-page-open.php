0c5908c356838b7fe70afbdc90ad8196318b30b0
Zane Rockenbaugh <zane@liquid-labs.com>
2016-03-08 15:33:31 -0600

    remove superslides

--- src/ui/documentation-default-page-open.php
+++ src/ui/documentation-default-page-open.php
@@ -12,7 +12,6 @@
 
   <link rel="shortcut icon" href="/style/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="/minify/?g=css" />
-  <link rel="stylesheet" href="/superslides.css" />
   <link rel="stylesheet" href="style/main.css" />
 
   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
